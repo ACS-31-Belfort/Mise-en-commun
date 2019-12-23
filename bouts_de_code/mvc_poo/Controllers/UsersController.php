510e31757e25371e9c0ecdf9e14113bc6880ff5c
lucas <lucas180395@gmail.com>
2019-12-23 23:03:24 +0100

    example of the index and show functions

--- bouts_de_code/mvc_poo/Controllers/UsersController.php
+++ bouts_de_code/mvc_poo/Controllers/UsersController.php
@@ -3,25 +3,33 @@
 class UsersController extends Controller
 {
 
-    private $args;
-    private $model;
-    protected $name = "Users";
+  private $args;
+  private $model;
+  protected $name = "Users";
 
-    public function __construct()
-    {
-        return $this->model = new UsersModel();
-    }
+  public function __construct()
+  {
+    return $this->model = new UsersModel();
+  }
 
 
-    public function index()
-    {
-        $data = $this->model->getAllUsers();
-        $this->render('users/index', $data);
-    }
+  public function index()
+  {
+    $data = $this->model->getAllUsers();
+    $this->render('users/index', $data);
+  }
 
-    public function delete()
-    {
-        $this->args = func_get_args();
-        echo "delete 1 user";
-    }
+  public function show()
+  {
+    // ignores other arguments. First one is assumed to be id of user.
+    $this->args = (int) func_get_args()[0];
+    $data = $this->model->getOneUser($this->args);
+    $this->render('users/show', $data);
+  }
+
+  public function delete()
+  {
+    $this->args = func_get_args();
+    echo "delete 1 user";
+  }
 }
--- bouts_de_code/mvc_poo/Views/users/index.php
+++ bouts_de_code/mvc_poo/Views/users/index.php
@@ -1,4 +1,5 @@
 Hello from User view :)
+This route and view are supposed to list all users.
 
 <?php 
 
